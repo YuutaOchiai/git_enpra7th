3d63b31b99cea9c6d64fc371fbb813a4e46e596c
YutaOchiai <yuuta001@akane.waseda.jp>
2023-06-14 10:28:38 +0900

    create hint system

--- enpra7th.py
+++ enpra7th.py
@@ -8,3 +8,17 @@ print(random_number)
 ans_number = int(input("解答を入力してください : "))
 
 #正解と不正解の出力
+if random_number == ans_number:
+    print("大正解!!")
+else:
+    print("不正解です。")
+
+print("以下にヒントを示します。次に表示される数字が解答の中に含まれます。")
+
+#ヒントを出力する
+def random_digit():
+    digit = rm.choice(str(random_number))
+    return digit
+random_digit()
+
+
